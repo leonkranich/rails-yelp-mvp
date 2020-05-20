0e99df6bdbe8f6646ca055bbf90b7ca78896a096
leon kranich <leon_kranich@yahoo.de>
2020-05-20 16:55:24 +0200

    Yeah it's working

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,8 @@
 class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
-
+  # def self.categories_array
+  #   ["chinese", "italian", "japanese", "french", "belgian"]
+  # end
   validates :name, presence: true
   validates :address, presence: true
   #validates :category, presence: true
